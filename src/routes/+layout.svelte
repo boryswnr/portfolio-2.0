f3bca43899474e888f0ec25c363db0f07b495212
boryswnr <borysweiner@gmail.com>
2022-11-24 13:20:04 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<script lang="Typescript">
+    import Hamburger from "../components/Hamburger.svelte";
+</script>
+
+<Hamburger />
+
+<nav class="navbar hidden" id="menu">
+    <ul class="nav-list">
+        <li class="nav-list-item">
+            <a href="/" class="link">
+            Home
+            </a>
+        </li>
+        <li class="nav-list-item">
+            <a href="/" class="link">
+            Skills
+            </a>
+        </li>
+        <li class="nav-list-item">
+            <a href="/" class="link">
+            Projects
+            </a>
+        </li>
+    </ul>
+</nav>
+
+<div class="main-wrapper">
+    <slot></slot>
+</div>
+
+<style lang="scss">
+    @import "../styling/layout.scss";
+</style>

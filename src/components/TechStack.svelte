4090ec459ed18743ed6f8255baf449974ff16b61
boryswnr <boryswnr@protonmail.com>
2022-12-08 11:50:56 +0100

    changed tech stack logos to monochromatic; changed their size and order;

--- src/components/TechStack.svelte
+++ src/components/TechStack.svelte
@@ -19,19 +19,19 @@
 		<img class="logo" src="tech-stack-svgs/React.svg" alt="React logo" />
 	</div>
 	<div class="logo-wrapper hide" style="--order: 4">
-		<img class="logo" src="tech-stack-svgs/HTML5.svg" alt="HTML logo" />
+		<img class="logo" src="tech-stack-svgs/Python.svg" alt="python logo" />
 	</div>
 	<div class="logo-wrapper hide" style="--order: 5">
-		<img class="logo" src="tech-stack-svgs/CSS3.svg" alt="CSS logo" />
-	</div>
-	<div class="logo-wrapper hide" style="--order: 6">
-		<img class="logo" src="tech-stack-svgs/Sass.svg" alt="Sass logo" />
-	</div>
-	<div class="logo-wrapper hide" style="--order: 7">
 		<img class="logo" src="tech-stack-svgs/Svelte.svg" alt="Svelte logo" />
 	</div>
+	<div class="logo-wrapper hide" style="--order: 6">
+		<img class="logo" src="tech-stack-svgs/HTML5.svg" alt="HTML logo" />
+	</div>
+	<div class="logo-wrapper hide" style="--order: 7">
+		<img class="logo" src="tech-stack-svgs/CSS3.svg" alt="CSS logo" />
+	</div>
 	<div class="logo-wrapper hide" style="--order: 8">
-		<img class="logo" src="tech-stack-svgs/Python.svg" alt="python logo" />
+		<img class="logo" src="tech-stack-svgs/Sass.svg" alt="Sass logo" />
 	</div>
 </div>
 

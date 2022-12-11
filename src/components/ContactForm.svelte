9f070edcd6d9406f6c9e8d298d8fbef95f37f8ab
boryswnr <boryswnr@protonmail.com>
2022-12-11 20:53:51 +0100

    take two on netlify form

--- src/components/ContactForm.svelte
+++ src/components/ContactForm.svelte
@@ -2,15 +2,18 @@
 	let name = '';
 	let email = '';
 	let message = '';
-	function handleSubmit() {}
+	function handleSubmit() {
+		console.log('submitted');
+	}
 </script>
 
-<form name="contact" netlify on:submit|preventDefault={handleSubmit}>
-	<input placeholder="Name" type="text" required bind:value={name} />
+<form name="contact" data-netlify="true" on:submit|preventDefault={handleSubmit}>
+	<input name="name" placeholder="Name" type="text" required bind:value={name} />
 
-	<input placeholder="E-mail" type="email" required bind:value={email} />
+	<input name="e-mail" placeholder="E-mail" type="email" required bind:value={email} />
 
 	<textarea
+		name="message"
 		class="message"
 		required
 		placeholder="What would you like to tell me?"

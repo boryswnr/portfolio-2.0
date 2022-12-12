be0c518aa0aa54bfcf83037660f9f8ba240a6491
boryswnr <boryswnr@protonmail.com>
2022-12-12 15:27:52 +0100

    debugging netlfy function

--- netlify/functions/mail.js
+++ netlify/functions/mail.js
@@ -3,29 +3,31 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.handler = async function (event) {
 	const { name, email, message } = JSON.parse(event.body);
-	console.log(event);
-	const response = await fetch('https://api.sendgrid.com/v3/', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`
-		},
-		body: JSON.stringify({
-			personalizations: [
-				{
-					to: [{ email: 'boryswnr@protonmail.com' }],
-					subject: [{ subject: 'New contact form message' }]
-				}
-			],
-			from: { email: 'boryswnr@protonmail.com' },
-			content: [
-				{
-					type: 'text/plain',
-					value: `Sender: ${name}, ${email}. Message: ${message}`
-				}
-			]
-		})
-	});
+	// console.log(event);
+	// const response = await fetch('https://api.sendgrid.com/v3/', {
+	// 	method: 'POST',
+	// 	headers: {
+	// 		'Content-Type': 'application/json',
+	// 		Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`
+	// 	},
+	// 	body: JSON.stringify({
+	// 		personalizations: [
+	// 			{
+	// 				to: [{ email: 'boryswnr@protonmail.com' }],
+	// 				subject: [{ subject: 'New contact form message' }]
+	// 			}
+	// 		],
+	// 		from: { email: 'boryswnr@protonmail.com' },
+	// 		content: [
+	// 			{
+	// 				type: 'text/plain',
+	// 				value: `Sender: ${name}, ${email}. Message: ${message}`
+	// 			}
+	// 		]
+	// 	})
+	// });
 
-	return response;
+	// return response;
+
+	return event.body;
 };

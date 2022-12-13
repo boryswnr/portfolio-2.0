0768025a2257038e277e7cadc38dae803600d615
boryswnr <boryswnr@protonmail.com>
2022-12-13 09:26:36 +0100

    added prerender target

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
 <script lang="ts">
 	import { intersectionObserver, angle } from '../utils';
 	import { onMount } from 'svelte';

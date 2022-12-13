08e77fe7bccfb762febdf2872764f442a5b854a8
boryswnr <boryswnr@protonmail.com>
2022-12-13 09:40:47 +0100

    deleted prerendering options

--- svelte.config.js
+++ svelte.config.js
@@ -8,12 +8,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
-		prerender: {
-			crawl: true,
-			entries: ['*'],
-			handleHttpError: 'warn'
-		}
+		adapter: adapter()
 	}
 };
 
